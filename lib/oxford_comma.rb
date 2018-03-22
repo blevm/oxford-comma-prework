f7160eb6b2b1000922917c4a306aca0360bc76f7
Michelle Blevins <mjblevins7@gmail.com>
2018-03-22 03:36:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   else
-    "#{array[0..-1].join(", ")} and #{array[-1]}"
+    "#{array[0..-2].join(", ")}, and #{array[-1]}"
   end
 end
